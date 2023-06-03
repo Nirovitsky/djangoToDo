e433642b144da09770503dcbefc7307ff425fb82
Nirovitsky <toymammedov01@gmail.com>
2023-06-03 19:17:46 +0800

    Fix login url issue

--- todo/settings.py
+++ todo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,7 +125,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 
-# LOGIN_URL = 'login/'
+LOGIN_URL = reverse_lazy('login')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
