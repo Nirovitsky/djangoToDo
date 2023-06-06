91409cfc71e59a4979b7a3a733da209bc52eef0f
Nirovitsky <toymammedov01@gmail.com>
2023-06-06 17:55:05 +0800

    Fix title max char. length

--- base/models.py
+++ base/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=30)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
