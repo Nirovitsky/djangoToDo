9ea3ce435348a02a7e9abcd8b30191bace583268
Nirovitsky <toymammedov01@gmail.com>
2023-06-06 18:01:41 +0800

    Fix title max char. length and remove complete button

--- base/models.py
+++ base/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=40)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
