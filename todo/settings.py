1357b4109e9d5632bd0cb0ce4ade624bec3d0e3a
Nirovitsky <toymammedov01@gmail.com>
2023-06-17 18:05:13 +0800

    Error validation and show error messages in register page

--- todo/settings.py
+++ todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7%_2t0(3!qef^4^)patpj+jq_j6m%nw7t%jaar_(&anq-u#bp9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+#
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
